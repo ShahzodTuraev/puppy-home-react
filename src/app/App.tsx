c881cfd8d070d9293f7a95c63454417a4e3411bc
shon <turayevshahzodbek@gmail.com>
2025-03-15 21:59:07 +0900

    fix: re set up

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../scss/home.scss";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { navbar } from "./lib/navbar";
 import Navbar from "./components/navbar";
 import NotFound from "../screens/NotFount";
@@ -8,42 +8,18 @@ import NotFound from "../screens/NotFount";
 const App = () => {
   return (
     <div className="root_container">
-      <Navbar />{" "}
-      {/* Ensure Navbar is outside Routes if it should always be visible */}
-      <Routes>
-        {navbar.map(({ path, element }, id) => (
-          <Route key={id} path={path} element={element} />
-        ))}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Navbar />}>
+            {navbar.map(({ path, element }, id) => {
+              return <Route key={id} path={path} element={element} />;
+            })}
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
 
 export default App;
-
-// import React from "react";
-// import "../scss/home.scss";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import { navbar } from "./lib/navbar";
-// import Navbar from "./components/navbar";
-// import NotFound from "../screens/NotFount";
-
-// const App = () => {
-//   return (
-//     <div className="root_container">
-//       <BrowserRouter>
-//         <Routes>
-//           <Route element={<Navbar />}>
-//             {navbar.map(({ path, element }, id) => {
-//               return <Route key={id} path={path} element={element} />;
-//             })}
-//           </Route>
-//           <Route path="*" element={<NotFound />} />
-//         </Routes>
-//       </BrowserRouter>
-//     </div>
-//   );
-// };
-
-// export default App;
